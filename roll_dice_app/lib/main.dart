7b811bb5aa38e6d34ab7e9deab63f770970339f1
unknown <sabasabzevari990@gmail.com>
2024-07-12 21:44:52 +0430

    Upload roll_dice_app on Github

---
+++ roll_dice_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
+
+// ctrl + space
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(
+            Color.fromARGB(255, 33, 5, 109), Color.fromARGB(255, 68, 21, 149)),
+      ),
+    ),
+  );
+}
